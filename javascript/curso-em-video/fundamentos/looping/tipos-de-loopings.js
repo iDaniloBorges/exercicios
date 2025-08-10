a35171fe754daced7f8f5cc41c3214ee8f929a7f
Danilo Borges <idaniloborges@gmail.com>
2025-08-10 18:16:42 -0300

    Add files via upload

---
+++ javascript/curso-em-video/fundamentos/looping/tipos-de-loopings.js
@@ -0,0 +1,21 @@
+//  Estrutura de repetição for
+console.log("Vai começar!");
+var c = 1;
+for (c = 1; c <= 4; c++) {
+  console.log(`${c}`);
+}
+console.log("Fim!");
+
+// Estrutura de repetição com teste no final
+var d = 1;
+do {
+  console.log(`Passo ${d}`);
+  d++;
+} while (c <= 10);
+
+// Estrutura de repetição com teste no início
+var e = 1;
+while (e < 6) {
+  console.log(`${e}`);
+  e++;
+}
---
+++ javascript/curso-em-video/fundamentos/object/object.js
@@ -0,0 +1,9 @@
+let amigo = {
+    nome: 'josé', 
+    genero:'M', 
+    peso:68.5,
+    engordar(p){
+
+    }
+}
+console.log(amigo.genero)
